7c2dc0922e38f3af49abdc4adadea8bdd9b76e3d
Vamsi <8751046+vmaddipa@users.noreply.github.com>
2026-08-08 13:57:48 -0700

    Add: products-of-array-discluding-self - submission-2

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-2.ts
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums: number[]): number[] {
+       const product = nums.reduce((acc, num)=> num*acc, 1);
+       const zeroCount = nums.reduce((acc, num)=> {
+        if(num === 0){
+            return acc + 1;
+        }
+        return acc
+       }, 0);
+       console.log(zeroCount, 'zeroCount');
+       if(zeroCount > 1) return new Array(nums.length).fill(0);
+
+       return nums.map((num) => {
+        if(num !== 0){
+           return  product/num;
+        } else {
+            const productExcludingZero = nums.reduce((acc, num)=> {
+                console.log(acc);
+                if(num === 0) return acc;
+                return acc*num;
+            }, 1);
+            console.log(productExcludingZero, 'productExcludingZero' )
+            return productExcludingZero;
+        }
+       });
+    }
+}
