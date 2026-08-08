4131af2c61a1f2ba36df1e11f22e3f91dc974e70
Vamsi <8751046+vmaddipa@users.noreply.github.com>
2026-08-08 13:12:20 -0700

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.ts
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs: string[]): string {
+        let res = '';
+        for(let str of strs){
+            res = res+`${str.length}#${str}`;
+
+        }
+        console.log(res)
+        return res;
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str: string): string[] {
+        const strs: string[] = [];
+        let index = 0;
+        while(index < str.length){
+            const delim = str.indexOf('#', index); 
+            const len = Number.parseInt(str.slice(index, delim));
+            strs.push(str.slice(delim+1, delim+len+1));
+            index=delim+len+1;
+        }
+
+        return strs;
+    }
+}
