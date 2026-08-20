05a2b893ac48eb61afab77f9ebb30ea96a277008
Vamsi <8751046+vmaddipa@users.noreply.github.com>
2026-08-20 13:37:14 -0700

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.ts
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices: number[]): number {
+        let l = 0;
+        let r = 1;
+        let maxP = 0;
+        while (r < prices.length) {
+            if (prices[l] > prices[r]) {
+                l = r;
+            } else {
+                maxP = Math.max(prices[r] - prices[l], maxP);
+            }
+            r++;
+        }
+        return maxP;
+    }
+}
