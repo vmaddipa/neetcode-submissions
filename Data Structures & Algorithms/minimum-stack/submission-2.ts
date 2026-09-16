1dc1ba3968ee03f754478e371608ace6aad10127
Vamsi <8751046+vmaddipa@users.noreply.github.com>
2026-09-16 10:43:28 -0700

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.ts
@@ -0,0 +1,39 @@
+class MinStack {
+    private stack = [];
+    private minStack = [];
+    constructor() {
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val: number): void {
+        this.stack.push(val);
+        const min = this.minStack.length == 0 ? val : this.minStack[this.minStack.length - 1];
+        this.minStack.push(Math.min(val, min));
+    }
+
+    /**
+     * @return {void}
+     */
+    pop(): void {
+        this.stack.pop();
+        this.minStack.pop();
+
+    }
+
+    /**
+     * @return {number}
+     */
+    top(): number {
+        return this.stack[this.stack.length - 1];
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin(): number {
+        return this.minStack[this.minStack.length - 1];
+    }
+}
